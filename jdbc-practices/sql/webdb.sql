d9d77e92704b2de5282e3119f871cdb4b57fc352
CHOSEOHEE <whtjgml2002@naver.com>
2025-09-09 11:58:51 +0900

    .

--- jdbc-practices/sql/webdb.sql
+++ jdbc-practices/sql/webdb.sql
@@ -54,9 +54,30 @@ select id, first_name, last_name, email from email order by id desc;
  --
  
  desc guestbook;
- 
+ alter table guestbook modify id int not null auto_increment;
  -- insert
  insert into guestbook values(null, '둘리', '1234', 'ㅎㅇ', now());
  
+ 
  -- findAll
- select id, name, date_format(reg_date, '%Y-%m-%d %h:%') from guestbook;
+ select * from guestbook order by id desc;
+ 
+ -- deleteByIdAndPassword
+ delete from guestbook where id = 1 and password = '1234';
+ 
+ 
+ --
+ -- mysite: user
+ --
+ 
+ desc user;
+ 
+ -- 회원가입
+ insert into user(name, email, password, gender, join_date) values ('둘리', 'dooly@gmail.com', password('1234'), 'male', current_date());
+ 
+ -- 회원리스트
+ select * from user;
+ 
+ -- 삭제
+ delete from user where id = 3;
+ 
